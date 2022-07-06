910470b4e9e416ac79157662098017c370f5608c
Lucas Heidrich Della Nina <108690201+lucasdellanina@users.noreply.github.com>
2022-07-06 10:52:32 -0300

    Add files via upload

---
+++ bissec.m
@@ -0,0 +1,18 @@
+# codigo para o metodo da bisseccao
+a = input ('Digite o valor do a: ');
+b = input ('Digite o valor do b: ');
+epsilon = input ('Digite a tolerancia: ');
+f = input ('Digite a funcao com o @(x): ');
+
+iteracao = 0;
+while abs(a-b) > epsilon
+    xest = (a + b)./2;
+    if (f(a).*f(xest))>0;
+      a = xest;
+    elseif (f(a).*f(xest))<0;
+      b = xest;
+    else
+      disp ('Raizes nao encontradas')
+    endif
+iteracao = iteracao +1;
+endwhile
